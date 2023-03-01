5a6f2894b1a3c1a25192777e94eaae882e25520d
mars <64494517+kas-kade@users.noreply.github.com>
2023-03-01 10:01:09 -0800

    Update ServoCommunication.py

--- Control/ServoCommunication.py
+++ Control/ServoCommunication.py
@@ -7,8 +7,18 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) 
     ser.reset_input_buffer()
 
+    x = 0
+
     while True:
         ser.write(b"Hello from Raspberry Pi!\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
+
+        while x == 0:
+            changeX = input("Insert a X value:")
+            changeY = input("Insert a Y value:")
+
+            ser.write(changeX + "\n")
+            ser.write(changeY + "\n")
+
         time.sleep(1)
