37aa41dc26197ec7710c515942c39d3c9e34660c
mars <64494517+kas-kade@users.noreply.github.com>
2023-03-01 10:04:28 -0800

    Create ServoCommunication.ino

---
+++ System/ServoCommunication.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+Servo servo1; // Up + Down
+Servo servo2; // Left + Right
+
+int posX = 0;
+int posY = 0;
+
+void setup() {
+  Serial.begin(9600);
+  servo1.attach(3);
+  servo2.attach(5);
+
+  servo1.write(0);
+  servo2.write(0);
+}
+
+void loop() {
+  readSerialPort();
+  delay(500);
+}
+
+void readSerialPort(){
+  if (Serial.available() > 0) {
+    int x = Serial.readStringUntil('\n');
+    int y = Serial.readStringUntil('\n');
+
+    posX += x;
+    posY += y;
+
+    servo1.(y);
+    servo2.(x);
+    
+    Serial.print("You sent me: ");
+    Serial.println(data);
+  }
+}
